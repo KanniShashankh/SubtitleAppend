e48bec953e8cea1e8dcd97a1b0eeaa0d85b9cabb
Shashankh Kanni <shashank.kanni@gmail.com>
2023-08-09 21:15:25 +0530

    Initial Commit

---
+++ SubtitleAppend/SubtitleAppendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubtitleAppendApp.swift
+//  SubtitleAppend
+//
+//  Created by Shashankh on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SubtitleAppendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
